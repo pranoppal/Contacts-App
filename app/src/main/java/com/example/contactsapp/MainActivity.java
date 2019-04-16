fcc4010e8b6caa6bc3497ecf6c944f350b7e40cf
pranoppal <pranoppal1006@gmail.com>
2019-04-16 23:06:52 +0530

    Added Contact Permission

--- app/src/main/java/com/example/contactsapp/MainActivity.java
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.contactsapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
     private DBhelper DbHelper;
     CustomCursorAdapter mCursorAdapter;
     SQLiteDatabase db,db1;
+    private int PermissionCode=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS)==PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"Contact Permission Granted",Toast.LENGTH_SHORT);
+        }
+        else
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_CONTACTS},PermissionCode);
 
         DbHelper = new DBhelper(this);
         ListView petListView = findViewById(R.id.list);
